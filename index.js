06df17eabb14930c4412e81ff2306a5577ec8aa2
Pete Bantock <peterbantock+gnome@gmail.com>
2019-07-09 20:53:20 +0100

    Changed cocktail overview page.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 const menu_item = Vue.component('cocktail-menu-item', {
   template: '<div class="container_fluid cocktail_details"><div class="d-flex justify-content-center mb-3"><div class="p-2">\
-  <h1>{{cocktail.name}}</h1>\
-  {{cocktail.served}}<br><br>\
+  <h1>{{cocktail.name}} Cocktail Recipe</h1>\
+  <br>\
+  How to make a {{cocktail.name}} cocktail.\
+  <br><br>\
   <h2>Ingredients</h2>\
   <ul>\
     <li v-for="ingredient in cocktail.ingredients">{{ingredient.measure}} {{ingredient.unit}} {{ingredient.ingredient}}</li>\
@@ -11,6 +13,8 @@ const menu_item = Vue.component('cocktail-menu-item', {
   <ul>\
     <li v-for="prep in cocktail.preparation">{{prep}}</li>\
   </ul>\
+  <br>\
+  Served: {{cocktail.served}}\
   </div></div></div>',
   watch: {
     '$route' (to, from){
