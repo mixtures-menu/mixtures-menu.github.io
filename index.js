0fe8d22506010365a7638daec655794c24f270b0
Pete Bantock <peterbantock+gnome@gmail.com>
2019-07-16 20:00:03 +0100

    SEO-ness

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const menu_item = Vue.component('cocktail-menu-item', {
   template: '<div class="container_fluid cocktail_details"><div class="d-flex justify-content-center mb-3"><div class="p-2">\
   <h1>{{cocktail.name}} Cocktail Recipe</h1>\
   <br>\
-  How to make a {{cocktail.name}} cocktail.\
+  Serve {{cocktail.served}} in a {{cocktail.drinkware}} and garnish with {{cocktail.garnish}}\
   <br><br>\
-  <h2>Ingredients</h2>\
+  <h2>What you need...</h2>\
   <ul>\
     <li v-for="ingredient in cocktail.ingredients">{{ingredient.measure}} {{ingredient.unit}} {{ingredient.ingredient}}</li>\
   </ul>\
-  <h2 v-if="cocktail.preparation">Preparation</h2>\
-  <ul>\
+  <h2 v-if="cocktail.preparation">How to make...</h2>\
+  <ol>\
     <li v-for="prep in cocktail.preparation">{{prep}}</li>\
-  </ul>\
-  <br>\
-  Served: {{cocktail.served}}\
+  </ol>\
   </div></div></div>',
   watch: {
     '$route' (to, from){
